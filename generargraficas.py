d94e364cc8ae040bb23352e6b3c749f7b6e0bc38
Hugo David Nogueda Hernandez <cdhugo9@gmail.com>
2025-12-01 23:37:55 -0600

    implementación dataset primera vista

--- generargraficas.py
+++ generargraficas.py
@@ -1,15 +1,35 @@
 from models.Bd import medicionesQuery, semanaQuery, data_sets
 from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
 
 semanas = semanaQuery()
+def operacion_dataset(dataset):
+    data_set = np.array(dataset)
+    size= data_set.size
+    data_set = np.reshape(data_set, shape=(1,size), order="C")
+    return data_set
 
-temperatura, humedad, fecha = data_sets(semanas[0][0])
+def establecer_dataset(semana):
+    temperatura, humedad, fecha = data_sets(semana)
+    temperatura_dataset = operacion_dataset(temperatura)
+    humedad_dataset = operacion_dataset(humedad)
+    fecha_dataset = operacion_dataset(fecha)
+    return temperatura_dataset, humedad_dataset, fecha_dataset
 
-temperatura_data_set = np.array(temperatura)
 
-s_temperatura = temperatura_data_set.size
 
-temperatura_dataset = np.reshape(temperatura_data_set, shape=(1,s_temperatura), order="C")
+temperatura_dataset, humedad_dataset, fecha_dataset = establecer_dataset(1)
 
-print(temperatura_dataset)
+df = pd.DataFrame({
+    'fecha': fecha_dataset[0],
+    'temperatura': temperatura_dataset[0],
+    'humedad': humedad_dataset[0]
+})
+
+df['fecha']=pd.to_datetime(df['fecha'],dayfirst=True)
+df_resample = df.resample('D', on='fecha').mean()
+
+plt.plot(df_resample.index, df_resample['temperatura'])
+plt.show()
+
